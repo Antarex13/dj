2aa821aa7e9fe7d13875c1d3bcde11be8c62f5f1
Antarex13 <vladislavmail264@gmail.com>
2022-03-24 23:07:33 +0600

    second

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+
+
 # from django.http import HttpResponse
 
 # Create your views here.
 def posts_list(request):
-    n = ['vlad', 'Belousov', 'Romanovich']
+    n = ['Vlad', 'Belousov', 'Romanovich']
     return render(request, 'blog/index.html', context={'names': n})
     # return HttpResponse('<h1>Hello World</h1>')
